17c678207fa8771aee89793ba6770f710a7f59ff
Funny-pixel765 <funnyrnd92@gmail.com>
2025-04-16 17:18:19 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static modull8.BankTransferConfig;
+
+namespace modull8
+{
+
+    //ngambil data data yang ada dari JSONNNYA
+    internal class BankTransferConfig
+    {
+        public string lang { get; set; }
+        public TransferClass transfer { get; set; }
+        public string[] methods { get; set; }
+        public ConfirmationClass confirmation { get; set; }
+
+        public class TransferClass
+        {
+            public int threshold { get; set; }
+            public int low_fee { get; set; }
+            public int high_fee { get; set; }
+        }
+        public class ConfirmationClass
+        {
+            public string en { get; set; }
+            public string id { get; set; }
+        }
+    }
+
+    internal class BankTransferConfigManager
+    {
+        public BankTransferConfig config;
+
+        //ini buat nyimpen file JSON
+        private static string file_path = Path.Combine(Directory.GetCurrentDirectory(), "bank_transfer_config.json");
+
+        //buat ngeread file JSON
+        private void ReadConfigFile()
+        {
+            string configJsonData = File.ReadAllText(file_path);
+            config = JsonSerializer.Deserialize<BankTransferConfig>(configJsonData);
+        }
+
+        //buat ngewrite
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions option = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            string jsonString = JsonSerializer.Serialize(config, option);
+            File.WriteAllText(file_path, jsonString);
+        }
+
+
+        //set default biar enak aja manggilnya
+        private void SetDefault()
+        {
+            config = new BankTransferConfig();
+
+            config.lang = "en";
+            TransferClass transfer = new();
+            transfer.threshold = 25000000;
+            transfer.low_fee = 6500;
+            transfer.high_fee = 15000;
+            config.transfer = transfer;
+            config.methods = new string[] { "RTO (real-time)", "SKN", "RTGS", "BI FAST" };
+            ConfirmationClass confirmation = new();
+            confirmation.en = "yes";
+            confirmation.id = "ya";
+            config.confirmation = confirmation;
+        }
+
+        public BankTransferConfigManager()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,112 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!");
+﻿using System;
+using modull8;
+
+namespace modull8
+{
+    class program
+    {
+        public static void Main(string[] args) 
+        {
+            //ngambil data
+            BankTransferConfigManager configApp = new BankTransferConfigManager();
+
+            //ngambil data lang
+            string lang = configApp.config.lang;
+            Console.WriteLine("Language = en & id");
+            string pilihBahasa = Console.ReadLine();
+            if (pilihBahasa == "en")
+            {
+                lang = "en";
+            }
+            else if (pilihBahasa == "id")
+            {
+                lang = "id";
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice, defaulting to English.");
+                lang = "en";
+            }
+
+            //-------------------------------
+
+            if (lang == "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer:");
+            }
+            else if (lang == "id")
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+            int nominal = int.Parse(Console.ReadLine());
+
+            //ya ni intinya buat ngatur duitnya
+            int duit =
+                (nominal > configApp.config.transfer.threshold)
+                ? configApp.config.transfer.high_fee
+                : configApp.config.transfer.low_fee;
+
+            //-----------------------------
+
+            if (lang == "en")
+            {
+                //nampilin fee
+                Console.WriteLine($"Transfer fee = {nominal}");
+                //total sama fee
+                Console.WriteLine($"Total amount = {duit + nominal}");
+                Console.WriteLine("");
+                Console.WriteLine("Select transfer method:");
+            }
+            else if (lang == "id")
+            {
+                //nampilin fee
+                Console.WriteLine($"Biaya transfer = {nominal}");
+                //total sama fee
+                Console.WriteLine($"Total biaya = {duit + nominal}");
+                Console.WriteLine("");
+                Console.WriteLine("Pilih metode transfer:");
+            }
+
+            //-------------------------------
+
+            for (int i = 0; i < configApp.config.methods.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + configApp.config.methods[i]);
+            }
+            int pilihanMethod = int.Parse(Console.ReadLine());
+
+            if (lang == "en")
+            {
+                //buat nampilin pilihannya
+                Console.WriteLine("You have choose: " + configApp.config.methods[pilihanMethod - 1] + " method");
+                Console.WriteLine("Please type 'yes' to confirm the transaction:");
+                string konfirmasiInggris = Console.ReadLine();
+                //utk ngecek data konfirmasi
+                if (konfirmasiInggris == configApp.config.confirmation.en)
+                {
+                    Console.WriteLine("The transfer is completed");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer is cancelled"); 
+                }
+            }
+            else if (lang == "id")
+            {
+                //buat nampilin pilihannya
+                Console.WriteLine("Anda memilih metode: " + configApp.config.methods[pilihanMethod - 1]);
+                Console.WriteLine("Ketik 'ya' untuk mengkonfirmasi transaksi:");
+                string konfirmasiIndo = Console.ReadLine();
+                //utk ngecek data konfirmasi
+                if (konfirmasiIndo == configApp.config.confirmation.id)
+                {
+                    Console.WriteLine("Proses transfer berhasil");
+                }
+                else
+                {
+                    Console.WriteLine("Transfer dibatalkan");
+                }
+            }
+        }
+    }
+}
